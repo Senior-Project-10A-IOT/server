7a03c5fb89342d5b836aa6738077bf466a282ff0
Zack <zphixon@gmail.com>
2022-02-24 15:07:36 -0500

    print message on startup

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import asyncio
 import websockets
 
-# test comment
+ip = '0.0.0.0'
+port = 8765
+
+print(f'now running server at {ip}:{port}')
 
 async def server(ws, path):
     print(f'connected to {path} from {ws.remote_address}')
@@ -20,7 +23,7 @@ async def server(ws, path):
             print(f'{ws.remote_address} disconnected')
             break
 
-start_server = websockets.serve(server, '0.0.0.0', 8765)
+start_server = websockets.serve(server, ip, port)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
