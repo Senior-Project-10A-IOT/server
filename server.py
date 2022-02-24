a57d19fb01067fd2e52005551f4e8d31851945b3
Zack <zphixon@gmail.com>
2022-02-24 14:42:44 -0500

    change ip

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ async def server(ws, path):
             print(f'{ws.remote_address} disconnected')
             break
 
-start_server = websockets.serve(server, 'localhost', 8765)
+start_server = websockets.serve(server, '0.0.0.0', 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
