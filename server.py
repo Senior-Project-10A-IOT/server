b58bb44c89bf4bcc0ee35a0de945b6e0a94703a6
Julia Varzari <jvarz@Julias-MacBook-Pro.local>
2022-03-01 13:01:52 -0500

    listen to multiple clients

--- server.py
+++ server.py
@@ -1,27 +1,45 @@
 import asyncio
 import websockets
+import os
+import random
 
 ip = '0.0.0.0'
 port = 8765
 
-print(f'now running server at {ip}:{port}')
+websocket_clients = set()
 
-async def server(ws, path):
-    print(f'connected to {path} from {ws.remote_address}')
-
-    i = 1
+async def handle_socket_connection(websocket, path):
+    """Handles the whole lifecycle of each client's websocket connection."""
+    websocket_clients.add(websocket)
+    print(f'New connection from: {websocket.remote_address} ({len(websocket_clients)} total)')
     try:
-        async for message in ws:
-            greeting = f'hello, {message} {i}'
-            i += 1
-            await ws.send(greeting)
-            print(f'send {greeting}')
-    except websockets.ConnectionClosedError:
+        # This loop will keep listening on the socket until its closed. 
+        async for raw_message in websocket:
+            print(f'Got: [{raw_message}] from socket [{id(websocket)}]')
+    except websockets.exceptions.ConnectionClosedError as cce:
         pass
+    finally:
+        print(f'Disconnected from socket [{id(websocket)}]...')
+        websocket_clients.remove(websocket)
 
-    print(f'disconnected {ws.remote_address}')
+async def broadcast_random_number(loop):
+    """Keeps sending a random # to each connected websocket client"""
+    while True:
+        for c in websocket_clients:
+            num = str(random.randint(10, 99))
+            print(f'Sending [{num}] to socket [{id(c)}]')
+            await c.send(num)
+        await asyncio.sleep(2)
 
-start_server = websockets.serve(server, ip, port)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        socket_server = websockets.serve(handle_socket_connection, ip, port)
+        print(f'Started socket server: {socket_server} ...')
+        loop.run_until_complete(socket_server)
+        loop.run_until_complete(broadcast_random_number(loop))
+        loop.run_forever()
+    finally:
+        loop.close()
+        print(f"Successfully shutdown [{loop}].")
 
