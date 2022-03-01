989f40ea4dedcc1b597ddf76fb3dc9c2e24e1575
Zack <zphixon@gmail.com>
2022-03-01 11:24:10 -0500

    use try, print disconnect message

--- server.py
+++ server.py
@@ -10,11 +10,16 @@ async def server(ws, path):
     print(f'connected to {path} from {ws.remote_address}')
 
     i = 1
-    async for message in ws:
-        greeting = f'hello, {message} {i}'
-        i += 1
-        await ws.send(greeting)
-        print(f'send {greeting}')
+    try:
+        async for message in ws:
+            greeting = f'hello, {message} {i}'
+            i += 1
+            await ws.send(greeting)
+            print(f'send {greeting}')
+    except websockets.ConnectionClosedError:
+        pass
+
+    print(f'disconnected {ws.remote_address}')
 
 start_server = websockets.serve(server, ip, port)
 asyncio.get_event_loop().run_until_complete(start_server)
