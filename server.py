e65e9c2cb56b09b1a1a301d60dba4f83baf4482c
Zack <zphixon@gmail.com>
2022-03-15 14:35:54 -0400

    refactor

--- server.py
+++ server.py
@@ -1,32 +1,40 @@
 import asyncio
 import websockets
-import os
-import random
 
-ip = '0.0.0.0'
-port = 8765
+IP = '0.0.0.0'
+PORT = 8765
+PHONE_PATH = '/phone'
+PI_PATH = '/raspi'
 
-websocket_clients = dict()
+phone_socket = None
 
 async def handle_socket_connection(websocket, path):
-    """Handles the whole lifecycle of each client's websocket connection."""
-    websocket_clients[path] = websocket
     print(f'New connection from: {websocket.remote_address} ({len(websocket_clients)} total) (path {path})')
+
+    if path == PHONE_PATH:
+        phone_socket = websocket
+
     try:
-        # This loop will keep listening on the socket until its closed. 
         async for raw_message in websocket:
-            if path == '/raspi' and websocket_clients['/phone'] != None:
-                await websocket_clients['/phone'].send("from server: " + raw_message)
+            if phone_socket != None and path == PI_PATH:
+                if type(raw_message) == str:
+                    await phone_socket.send("from server: " + raw_message)
+                elif type(raw_message) == bytes:
+                    # TODO
+                    pass
+
     except websockets.exceptions.ConnectionClosedError as cce:
         pass
+
     finally:
         print(f'Disconnected from socket [{id(websocket)}]...')
-        websocket_clients[path] = None
+        if path == PHONE_PATH:
+            phone_socket = None
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     try:
-        socket_server = websockets.serve(handle_socket_connection, ip, port)
+        socket_server = websockets.serve(handle_socket_connection, IP, PORT)
         print(f'Started socket server: {socket_server} ...')
         loop.run_until_complete(socket_server)
         loop.run_forever()
