293f723a4981aeb2639f99fca5eb6e0d9dc350cb
Zack <zphixon@gmail.com>
2022-04-14 12:21:35 -0400

    refactor

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+import json
 
 IP = '0.0.0.0'
 PORT = 8765
@@ -7,31 +8,45 @@ PHONE_PATH = '/phone'
 PI_PATH = '/raspi'
 
 phone_socket = None
+pi_socket = None
+
+async def from_phone(message):
+    global pi_socket
+    print(f'message from phone: {message}')
+    pass
+
+async def from_pi(message):
+    global phone_socket
+    await phone_socket.send(f'from server: {message}')
+    pass
 
 async def handle_socket_connection(websocket, path):
     global phone_socket
+    global pi_socket
     print(f'New connection from: {websocket.remote_address} (path {path})')
 
     if path == PHONE_PATH:
         phone_socket = websocket
+    if path == PI_PATH:
+        pi_socket = websocket
 
     try:
-        async for raw_message in websocket:
-            if type(raw_message) == str:
-                print(f'message from {path}: {raw_message}')
-            if phone_socket != None and path == PI_PATH:
-                if type(raw_message) == str:
-                    await phone_socket.send("from server: " + raw_message)
-                elif type(raw_message) == bytes:
-                    await phone_socket.send(raw_message)
-
+        async for message in websocket:
+            if phone_socket != None and pi_socket != None:
+                if path == PHONE_PATH:
+                    from_phone(message)
+                elif path == PI_PATH:
+                    from_pi(message)
+            else:
+                print(f'dropped message from {path}')
     except websockets.exceptions.ConnectionClosedError as cce:
         pass
-
     finally:
         print(f'Disconnected from socket {path}...')
         if path == PHONE_PATH:
             phone_socket = None
+        if path == PI_PATH:
+            pi_socket = None
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
